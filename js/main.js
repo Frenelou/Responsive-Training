a4e5044eda2a96980e8f51cbd13ca4f694f55ab0
Flora <Flora.rene@gmail.com>
2016-01-09 00:15:36 +0100

    first draft

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(function() {
+    setItemsHeight('.hexagon', '#social-icons a');
+});
+$(window).resize(function() {
+    setItemsHeight('.hexagon', '#social-icons a');
+});
+
+function setItemsHeight(target) {
+    for (var i = 0, j = arguments.length; i < j; i++) {
+
+      switch (arguments[i]) {
+        case '.hexagon':
+        ratio = 1.15;
+        break;
+        default:
+        ratio = 1;
+      }
+
+        target_width = Math.floor($(arguments[i]).width());
+        $(arguments[i]).css('height', target_width * ratio) + 'px';
+
+        if (arguments[i] === '.hexagon') {
+            margin_bottom = target_width / 4;
+            $(arguments[i]).css({
+                'margin-bottom': '-' + margin_bottom + 'px',
+                'margin-right': margin_bottom / 4
+            });
+        }else if (arguments[i] === '#social-icons a') {
+          $(arguments[i]).css('line-height', target_width + 'px' );
+        }
+
+    }
+}
