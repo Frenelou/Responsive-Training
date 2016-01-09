70b5ad4ba061ab48400e64e811e9bddfb11cb768
Flora <Flora.rene@gmail.com>
2016-01-10 00:28:23 +0100

    cleaning up

--- js/main.js
+++ js/main.js
@@ -4,17 +4,29 @@ $(function() {
 $(window).resize(function() {
     setItemsHeight('.hexagon', '#social-icons a');
 });
-$(".navbar-nav a[href^=#]").click(function(e) { e.preventDefault(); var dest = $(this).attr('href'); console.log(dest); $('html,body').animate({ scrollTop: $(dest).offset().top - 100 }, 'slow'); });
+
+//Navigation
+$(".navbar-nav a[href^=#]").click(function(e) {
+    e.preventDefault();
+    var dest = $(this).attr('href');
+    $('.active').removeClass('active');
+    $(this).parent().addClass('active');
+    $('html,body').animate({
+        scrollTop: $(dest).offset().top - 70
+    }, 'slow');
+});
+
+
 function setItemsHeight(target) {
     for (var i = 0, j = arguments.length; i < j; i++) {
 
-      switch (arguments[i]) {
-        case '.hexagon':
-        ratio = 1.15;
-        break;
-        default:
-        ratio = 1;
-      }
+        switch (arguments[i]) {
+            case '.hexagon':
+                ratio = 1.15;
+                break;
+            default:
+                ratio = 1;
+        }
 
         target_width = Math.floor($(arguments[i]).width());
         $(arguments[i]).css('height', target_width * ratio) + 'px';
@@ -25,8 +37,8 @@ function setItemsHeight(target) {
                 'margin-bottom': '-' + margin_bottom + 'px',
                 'margin-right': margin_bottom / 4
             });
-        }else if (arguments[i] === '#social-icons a') {
-          $(arguments[i]).css('line-height', target_width + 'px' );
+        } else if (arguments[i] === '#social-icons a') {
+            $(arguments[i]).css('line-height', target_width + 'px');
         }
 
     }
